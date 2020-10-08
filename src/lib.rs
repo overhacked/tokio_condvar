c1c0ead08bf7da66b44bb65432807f4e0efe38db
Ross Williams <ross@ross-williams.net>
2020-10-07 21:20:06 -0400

    Generic version

--- src/lib.rs
+++ src/lib.rs
@@ -1,84 +1,83 @@
-use std::fmt;
+use std::fmt::Debug;
 use std::time::Duration;
-use tokio;
 use tokio::sync::watch::{self, Sender, Receiver};
 use tokio::time::{timeout as tokio_timeout, Elapsed};
 
 #[derive(Debug)]
-pub struct Waiter {
-    ready_tx: Sender<bool>,
-    ready_rx: Receiver<bool>,
+pub struct Waiter<T> {
+    ready_tx: Sender<T>,
+    ready_rx: Receiver<T>,
 }
 
-impl fmt::Display for Waiter {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        let ready_str = if self.is_ready() { "Ready" } else { "Not Ready" };
-        write!(f, "{}", ready_str)
-    }
-}
-
-impl Waiter {
-    pub fn new(is_ready: bool) -> Self {
-        let (tx, rx) = watch::channel(is_ready);
+impl<T> Waiter<T>
+    where T: Clone + Debug + PartialEq + Send + Sync
+{
+    pub fn new(init: T) -> Self {
+        let (tx, rx) = watch::channel(init);
         Waiter {
             ready_tx: tx,
             ready_rx: rx,
         }
     }
 
-    pub fn set(&self, is_ready: bool) {
-        self.ready_tx.broadcast(is_ready).unwrap();
+    pub fn set(&self, value: T) {
+        self.ready_tx.broadcast(value).unwrap();
     }
 
-    pub fn set_ready(&self) {
-        self.set(true)
-    }
-
-    pub fn set_unready(&self) {
-        self.set(false)
-    }
-
-    pub fn is_ready(&self) -> bool {
-        *self.ready_rx.borrow()
-    }
-
-    pub async fn wait(&self, is_ready: bool) {
+    pub async fn wait(&self, desired: &T) {
         let mut rx = self.ready_rx.clone();
         loop {
             match rx.recv().await {
                 None => panic!("self.ready_tx should not be dropped"),
-                Some(v) if v == is_ready => break,
+                Some(ref v) if v == desired => break,
                 Some(_) => continue,
             }
         }
     }
 
-    pub async fn wait_timeout(&self, timeout: Duration, is_ready: bool)
+    pub async fn wait_timeout(&self, timeout: Duration, desired: &T)
         -> Result<(), Elapsed>
     {
-        tokio_timeout(timeout, self.wait(is_ready)).await?;
+        tokio_timeout(timeout, self.wait(desired)).await?;
         Ok(())
     }
 
-    pub async fn wait_ready(&self) {
-        self.wait(true).await
+}
+
+impl<C: Copy> Waiter<C> {
+    pub fn value(&self) -> C {
+        *self.ready_rx.borrow()
+    }
+}
+
+impl Waiter<bool> {
+    pub fn set_true(&self) {
+        self.set(true)
     }
 
-    pub async fn wait_unready(&self) {
-        self.wait(false).await
+    pub fn set_false(&self) {
+        self.set(false)
+    }
+
+    pub async fn wait_true(&self) {
+        self.wait(&true).await
+    }
+
+    pub async fn wait_false(&self) {
+        self.wait(&false).await
     }
 
     pub async fn wait_ready_timeout(&self, timeout: Duration)
         -> Result<(), Elapsed>
     {
-        self.wait_timeout(timeout, true).await?;
+        self.wait_timeout(timeout, &true).await?;
         Ok(())
     }
 
-    pub async fn wait_unready_timeout(&self, timeout: Duration)
+    pub async fn wait_false_timeout(&self, timeout: Duration)
         -> Result<(), Elapsed>
     {
-        self.wait_timeout(timeout, false).await?;
+        self.wait_timeout(timeout, &false).await?;
         Ok(())
     }
 }
